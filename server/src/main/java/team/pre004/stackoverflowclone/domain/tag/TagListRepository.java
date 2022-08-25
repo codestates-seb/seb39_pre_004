50296aec3d7f9101bf3f4a2daf5f10c9e7b49c8d
ward <bonuswon@gmail.com>
2022-08-26 02:39:19 +0900

    feat: AWS RDS MariaDB Create Tables / Spring Boot JPA domain mapping for Table (#31 close)

---
+++ server/src/main/java/team/pre004/stackoverflowclone/domain/tag/TagListRepository.java
@@ -0,0 +1,6 @@
+package team.pre004.stackoverflowclone.domain.tag;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagListRepository extends JpaRepository<TagList, Long> {
+}
---
+++ server/src/main/java/team/pre004/stackoverflowclone/domain/user/User.java
@@ -0,0 +1,36 @@
+package team.pre004.stackoverflowclone.domain.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@NoArgsConstructor
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    private String bio;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modDate;
+}
