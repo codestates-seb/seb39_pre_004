4790a72d5849ef94c3777eb94084b99aa4a78429
ward <bonuswon@gmail.com>
2022-08-25 17:26:36 +0900

    test: git push test

--- server/src/main/java/team/pre004/stackoverflowclone/web/controller/UserController.java
+++ server/src/main/java/team/pre004/stackoverflowclone/web/controller/UserController.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/v1/users")
+@RequestMapping("/users")
 public class UserController {
 
     @PostMapping("/{username_id}")
