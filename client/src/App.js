b17ef2d0af9fdff588b2335d6cf19a2a8b011deb
string_main <yhj2756@naver.com>
2022-08-26 17:05:15 +0900

    feat: 공통 레이아웃 (헤더, 푸터, 메뉴바) 마크업 (#36)

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CommonLayout from './components/CommonLayout';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: #ffffff;
+  }
+`;
 
 const App = () => {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<CommonLayout />}>
           <Route path="/login" element={<div>login</div>}></Route>
--- client/src/components/CommonLayout.js
+++ client/src/components/CommonLayout.js
@@ -1,13 +1,62 @@
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+
+const HeaderBlock = styled.div`
+  padding: 10px;
+  border: 1px solid;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #f8f9f9;
+`;
+
+const FooterBlock = styled.div`
+  padding: 10px;
+  border: 1px solid;
+  display: flex;
+  background: #232629;
+  color: #ffffff;
+`;
+
+const NavBlock = styled.div`
+  padding: 10px;
+  background-color: aliceblue;
+  border: 1px solid;
+`;
 
 const CommonLayout = () => {
   return (
     <div>
-      <header>header</header>
-      <main>
-        <Outlet />
-      </main>
-      <footer>footer</footer>
+      <header>
+        <HeaderBlock>
+          <a href="*">
+            <span>Stack Overflow</span>
+          </a>
+          <input type="search" />
+          <button>Log in</button>
+          <button>Sign up</button>
+        </HeaderBlock>
+      </header>
+
+      <div>
+        <nav>
+          <NavBlock>nav</NavBlock>
+        </nav>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+
+      <footer>
+        <FooterBlock>
+          <div>
+            <div>footer logo</div>
+            <div>footer nav</div>
+            <div>footer copyright</div>
+          </div>
+        </FooterBlock>
+      </footer>
     </div>
   );
 };
