5fb11d0ae58c37c1fd9612eea12cfb4e95973b14
ward <bonuswon@gmail.com>
2022-08-31 02:29:33 +0900

    feat: AnswerService 기능 정의 (#83)

---
+++ server/src/main/java/team/pre004/stackoverflowclone/service/AnswerService.java
@@ -0,0 +1,23 @@
+package team.pre004.stackoverflowclone.service;
+
+import team.pre004.stackoverflowclone.domain.post.entity.Answer;
+import team.pre004.stackoverflowclone.domain.post.entity.Question;
+
+import java.util.Optional;
+import java.util.Set;
+
+public interface AnswerService {
+
+    Set<Question> findAll();
+    Answer save(Answer answer);
+    Answer update(Long id, Answer answer);
+    Optional<Answer> findById(Long id);
+    void deleteById(Long id);
+    boolean acceptAnswer(Long userId, Long answerId);
+    boolean acceptAnswerUndo(Long userId, Long answerId);
+    Integer selectLikeUp(Long userId, Long answerId);
+    Integer selectLikeUpUndo(Long userId, Long answerId);
+    Integer selectLikeDown(Long userId, Long answerId);
+    Integer selectLikeDownUndo(Long userId, Long answerId);
+
+}
