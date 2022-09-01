47935f7ee772845fd89d53ce882d2786cd97a64b
ward <bonuswon@gmail.com>
2022-09-01 23:06:33 +0900

    feat: security 로그인 후 인증처리 완료

---
+++ server/src/main/java/team/pre004/stackoverflowclone/service/AuthService.java
@@ -0,0 +1,9 @@
+package team.pre004.stackoverflowclone.service;
+
+import team.pre004.stackoverflowclone.dto.auth.LogInDto;
+import team.pre004.stackoverflowclone.dto.auth.SignUpDto;
+
+public interface AuthService {
+    void signUp(SignUpDto signUpDto);
+    void login(LogInDto logInDto);
+}
