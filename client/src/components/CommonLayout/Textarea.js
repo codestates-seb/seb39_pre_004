ae126a32c6d2b0ada08365710c841a0969cb225b
dulce04 <aocl333@nate.com>
2022-09-03 01:27:14 +0900

    [FE] 게시판 추가 페이지 데이터 적용(#120)

---
+++ client/src/components/CommonLayout/Textarea.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Textarea = styled.textarea`
+  width: 100%;
+  height: 250px;
+  border: 1px solid var(--gray);
+  resize: none;
+  padding: 10px;
+  margin-bottom: 20px;
+`;
+
+export default Textarea;
