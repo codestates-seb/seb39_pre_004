0a9c86ac5fe82848c7b976d959ee0df4b24ef9ba
string_main <yhj2756@naver.com>
2022-09-03 18:27:40 +0900

    feat: 로그인 post 요청 및 JWT 토큰 localStorage 저장 (#119)

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -3,6 +3,10 @@ import Subtitle from '../components/Subtitle';
 import BlueButton from '../components/Bluebutton';
 import LoginSocial from '../components/SocialButton/LoginSocial';
 import Input from '../components/Input';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { userLogin } from '../slices/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Container = styled.div`
   width: 100%;
@@ -24,24 +28,61 @@ const LoginBlock = styled.div`
   position: relative;
   border: 1px solid var(--light--gray);
   padding: 20px;
+
+  span {
+    font-size: 15px;
+    color: var(--red);
+    display: inline-block;
+    margin-bottom: 10px;
+  }
 `;
 
 const LoginPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { success } = useSelector((state) => state.user);
+
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+  const data = { email: loginEmail, password: loginPassword };
+
+  useEffect(() => {
+    if (success) {
+      navigate('/');
+    }
+  }, [success]);
+
+  const onLoginHandler = (e) => {
+    e.preventDefault();
+    dispatch(userLogin(data));
+  };
+
   return (
     <>
       <Container>
         <LoginSocial />
-        <LoginBlock>
-          <div>
-            <Subtitle>Email</Subtitle>
-            <Input />
-          </div>
-          <div>
-            <Subtitle>Password </Subtitle>
-            <Input />
-          </div>
-          <BlueButton>Log in</BlueButton>
-        </LoginBlock>
+        <form onSubmit={(e) => onLoginHandler(e)}>
+          <LoginBlock>
+            <div>
+              <Subtitle>Email</Subtitle>
+              <Input
+                type="email"
+                onChange={(e) => setLoginEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <Subtitle>Password </Subtitle>
+              <Input
+                type="password"
+                onChange={(e) => setLoginPassword(e.target.value)}
+                required
+              />
+              {/* {!isLogin && <span>Email 또는 Password를 확인하세요.</span>} */}
+            </div>
+            <BlueButton type="submit">Log in</BlueButton>
+          </LoginBlock>
+        </form>
       </Container>
     </>
   );
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import commentReducer from '../slices/commentSlice';
 import postReducer from '../slices/postSlice';
 import addReducer from '../slices/addSlice';
+import userReducer from '../slices/userSlice';
 
 export const store = configureStore({
   reducer: {
     comment: commentReducer,
     singlePost: postReducer,
     add: addReducer,
+    user: userReducer,
     /* 슬라이스에 매칭되는 리듀서를 넣어주세요. */
   },
 });
